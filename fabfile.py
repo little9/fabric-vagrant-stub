ac3ba5bb326d2bf230793a3766cc3e539a7de0e6
Jamie Little <j.little@miami.edu>
2016-08-12 12:03:05 -0400

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,10 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-def vagrant(port):
+def vagrant(port): 
     env.user = 'vagrant'
+    # Give the port as an arugment -- this changes depending on the VM
     env.hosts = ['127.0.0.1:%s' % port]
-    # use vagrant ssh key
+    # use vagrant ssh key // via http://sysadminpy.com/sysadmin/2011/04/30/use-fabric-on-vagrant-instances/ updated to vagrant-ssh
     result = local('vagrant ssh-config | grep IdentityFile', capture=True)
     env.key_filename = result.split()[1]
